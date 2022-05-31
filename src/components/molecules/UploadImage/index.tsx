86b6abf99ffd59296c5303a5ff82789b704d37d6
Mateus Severiano <mateusbatatais@gmail.com>
2022-05-31 18:22:51 -0300

    uploadimage to storage

--- src/components/molecules/UploadImage/index.tsx
+++ src/components/molecules/UploadImage/index.tsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
+import getBase64 from "../../../utils/getBase64";
+
 const UploadImage = () => {
-  const submit = (event: any) => {};
-  const onChange = (e: any) => {};
+  const initialValues = JSON.parse(localStorage.getItem("images") || "[]");
+  const [preview, setPreview] = useState<any>();
+  const [list, setList] = useState(initialValues || []);
+
+  const submit = (event: any) => {
+    event.preventDefault();
+    const newList = [...list];
+    newList.push(preview);
+    setList(newList);
+    localStorage.setItem("images", JSON.stringify(newList));
+  };
+
+  const onChange = (e: any) => {
+    const file = e.target.files[0];
+    getBase64(file).then((base64) => {
+      setPreview(base64);
+    });
+  };
+
   return (
     <form onSubmit={submit}>
       <div className="mb-3">
@@ -12,8 +32,11 @@ const UploadImage = () => {
           onChange={onChange}
         />
       </div>
+      {preview && (
+        <img src={preview} width="200" alt="preview" className="mb-3" />
+      )}
 
-      <button type="submit" className="btn btn-primary">
+      <button type="submit" className="btn btn-primary d-flex">
         SEND
       </button>
     </form>
